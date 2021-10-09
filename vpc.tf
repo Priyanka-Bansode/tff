b9068c0227ad51acac6df7970afc97daf9e8589d
Priyanka-Bansode <79906954+Priyanka-Bansode@users.noreply.github.com>
2021-10-09 16:14:37 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "demo123" {
+resource "aws_vpc" "first" {
   cidr_block       = "70.70.0.0/16"
   instance_tenancy = "default"
 
@@ -9,7 +9,7 @@ resource "aws_vpc" "demo123" {
 
 
 resource "aws_subnet" "demo1" {
-  vpc_id     = aws_vpc.demo123.id
+  vpc_id     = aws_vpc.first.id
   cidr_block = "70.70.1.0/28"
 
   tags = {
@@ -19,7 +19,7 @@ resource "aws_subnet" "demo1" {
 
 
 resource "aws_subnet" "demo2" {
-  vpc_id     = aws_vpc.demo123.id
+  vpc_id     = aws_vpc.first.id
   cidr_block = "70.70.1.16/28"
 
   tags = {
@@ -27,14 +27,14 @@ resource "aws_subnet" "demo2" {
   }
 }
 resource "aws_internet_gateway" "massgw" {
-  vpc_id = aws_vpc.demo123.id
+  vpc_id = aws_vpc.first.id
 
   tags = {
     Name = "mass"
   }
 }
 resource "aws_route_table" "Pu_RT" {
-  vpc_id = aws_vpc.demo123.id
+  vpc_id = aws_vpc.first.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -46,5 +46,5 @@ resource "aws_route_table" "Pu_RT" {
   }
 }
 resource "aws_route_table" "Private-RT" {
-  vpc_id = aws_vpc.demo123.id
+  vpc_id = aws_vpc.first.id
   }
